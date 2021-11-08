d4e2b506c129598a24339bbff5f1850707abf436
olix3001 <olimioli9@gmail.com>
2021-11-08 19:01:40 +0100

    Initial commit with spring boot

---
+++ src/test/java/com/secretmessage/smserver/SmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secretmessage.smserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
