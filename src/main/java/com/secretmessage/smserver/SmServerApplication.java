b934540ae625c41e1e1c8692cc55a6d30c0fd107
jj15warrior <janek302010@gmail.com>
2021-11-13 23:46:54 +0100

    commit does not work???

--- src/main/java/com/secretmessage/smserver/SmServerApplication.java
+++ src/main/java/com/secretmessage/smserver/SmServerApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.secretmessage.smserver.UserType.type.Developer;
 
 
 @SpringBootApplication
@@ -25,7 +24,13 @@ public class SmServerApplication {
 	@GetMapping("/register")
 	public ResponseEntity<String> register(@RequestHeader("login") String login, @RequestHeader("pass") String pass) {
 		users.add(new User(
-
+			id = users.length()+1,
+			createdAt = 
+			discriminator
+			username
+			language
+			isDeveloper
+			user_type
 		))
 		return String.format(GetHash.token());
 	}
